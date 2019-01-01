b77efde06afebf89fbfdd3286175ea754f32cfb5
SzczepanGortowski <szczepan.gortowski@github.com>
2019-01-01 20:02:06 +0100

    Interfaces and entities definition

---
+++ Gortowski.Vineyard.Core/Enum/VineType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+public enum VineType
+
+{
+
+    Wytrawne    = 1,
+    Półwytrawne = 2,
+    Półsłodkie  = 3,
+    Słodkie     = 4,
+    All         = 0
+
+}
---
+++ Gortowski.Vineyard.DAO/Entities/Vine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+public class Vine : IVine
+{
+    //TODO
+    public string Name { get; set; }
+    public VineType VineType { get; set; }
+
+}
